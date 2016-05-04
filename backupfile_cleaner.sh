c834472363b33b82032b42bcd5035f0777121dd8
Anorpi <5262122@gmail.com>
2016-05-04 17:42:19 +0800

    create backupfile_cleaner.sh and delete cleanbackup.sh

---
+++ backupfile_cleaner.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+#--------------------------  
+#   Name:clean_backupfile
+#   Type:function
+#   Language:bash shell  
+#   Date:2016-4-22  
+#   Author:Jia Jianjun  
+#   Email:jiajianjun(at)time2011.com  
+#--------------------------
+#Set clean file's type
+
+file_type=gz
+##Check path.
+#if [ -d ../webapps ];then
+#	PWD=`pwd`
+#else
+#	echo "Not in ./webapps path,plase check."
+#	exit
+#fi
+
+#Get old files list.
+old_list=`ls -t $PWD/*.$file_type 2>/dev/null|tail -n +4`
+echo $old_list
+
+#Delete old files if have.
+if [ -z "$old_list" ];then
+        exit;
+else
+        rm -rf $old_list
+fi
