3edaf4d7f62194f69544b29a67fd7531c4711e27
Anorpi <5262122@gmail.com>
2016-05-07 14:24:57 +0800

    add generate_file.sh

---
+++ generate_file.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#an example,touch a new test.txt file with some line.
+cat <<EOF>test.txt
+#one line
+#two line
+#three line
+servername 8.8.8.8
+EOF
+
+#another example,append line in text.txt file
+cat <<EOF>>test.txt
+#just append a new line
+EOF
